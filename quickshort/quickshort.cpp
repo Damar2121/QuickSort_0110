f25370f01b3712cdc377a60934b7aa8707748628
Damar2121 <damar.vivoy51@gmail.com>
2024-03-29 13:51:29 +0700

    search for an element greater than ρίνοt

--- quickshort/quickshort.cpp
+++ quickshort/quickshort.cpp
@@ -54,3 +54,7 @@ void q_short(int low, int high)
 
     while (i <= j)//step 10
     {
+        while ((arr[i] <= pivot) && (i <= high)) // step 5
+        {
+            i++; //step 6
+            cmp_count++;
